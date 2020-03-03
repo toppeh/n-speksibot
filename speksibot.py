3632110b247d9450b94e862f64cd418fc05cbf9f
Henri Kiiski <henri.kiiski@tuni.fi>
2020-03-03 19:43:33 +0200

    Bottiin lisätty /help ja /changename komennot, lisätty virheenhallintaa ja tehty hieman järkevämmin asioita.

--- speksibot.py
+++ speksibot.py
@@ -11,16 +11,26 @@ with open('groups.txt') as f:
     f.close()
 
 
+def writeToFile():
+    with open('groups.txt', 'w') as fp:
+        json.dump(groups, fp)
+        fp.close()
+
+
 def addGroup(update: Update, context: CallbackContext):
+    name = update.message.text[10:]
+    if ' ' in name:
+        context.bot.sendMessage(chat_id=update.message.chat_id, text=f'Ryhmän nimessä ei saa olla välilyöntiä.')
+        return
     for chatName in groups:
         if groups[chatName] == update.message.chat_id:
             context.bot.sendMessage(chat_id=update.message.chat_id, text=f'Ryhmä on jo lisätty nimellä {chatName}')
             return
-    name = update.message.text[10:]
+        if chatName == name:
+            context.bot.sendMessage(chat_id=update.message.chat_id, text=f'Ryhmä nimeltä {chatName} on jo lisätty')
+            return
     groups[name] = update.message.chat_id
-    with open('groups.txt', 'w') as fp:
-        json.dump(groups, fp)
-        fp.close()
+    writeToFile()
     context.bot.sendMessage(chat_id=update.message.chat_id, text=f'Ryhmä lisätty nimellä {name}')
 
 
@@ -35,28 +45,66 @@ def printGroups(update: Update, context: CallbackContext):
 
 
 def forwardMsg(update: Update, context: CallbackContext):
-    if update.message.text[0] != '@':
-        return
-    strTuple = update.message.text.partition(' ')
-    if len(strTuple[1]) == 0:
-        return
-    if strTuple[0][1:] not in groups:
-        context.bot.sendMessage(chat_id=update.message.chat_id, text=f'Ryhmää nimeltä {strTuple[0][1:]} ei löytynyt.'
-                                                                     f' Klikkaa /list näkeäksesi listan tietämistäni'
-                                                                     f' ryhmistä')
-        return
-    try:
-        context.bot.forwardMessage(chat_id=groups[strTuple[0][1:]], from_chat_id=update.message.chat_id,
-                                   message_id=update.message.message_id)
-    except TelegramError:
-        context.bot.sendMessage(chat_id=update.message.chat_id, text='Jokin meni pieleen :/')
+    name = update.message.parse_entity(update.message.entities[0])[1:]
+    if name in groups:
+        try:
+            context.bot.forwardMessage(chat_id=groups[name], from_chat_id=update.message.chat_id,
+                                       message_id=update.message.message_id)
+            context.bot.sendMessage(chat_id=update.message.chat_id, text=f'Viesti välitetty.')
+        except TelegramError:
+            context.bot.sendMessage(chat_id=update.message.chat_id, text='Jokin meni pieleen :/')
 
 
-updater = Updater(token=config.TOKENN, use_context=True)
+def replyForward(update: Update, context: CallbackContext):
+    name = update.message.parse_entity(update.message.entities[0])[1:]
+    if name in groups:
+        try:
+            context.bot.forward_message(chat_id=groups[name], from_chat_id=update.message.chat_id,
+                                        message_id=update.message.reply_to_message.message_id)
+        except TelegramError:
+            context.bot.sendMessage(chat_id=update.message.chat_id, text='Jokin meni pieleen :/')
+
+
+def changeName(update: Update, context: CallbackContext):
+    newName = update.message.text[12:]
+    if ' ' in newName:
+        context.bot.sendMessage(chat_id=update.message.chat_id, text=f'Ryhmän nimessä ei saa olla välilyöntiä.')
+        return
+    elif newName in groups.keys():
+        context.bot.sendMessage(chat_id=update.message.chat_id, text=f'Nimi on jo käytössä.')
+        return
+    elif len(newName) == 0:
+        context.bot.sendMessage(chat_id=update.message.chat_id, text=f'Uusi nimi ei saa olla tyhjä')
+        return
+    for name, id in groups.items():
+        if id == update.message.chat_id:
+            groups[newName] = update.message.chat_id
+            del groups[name]
+            writeToFile()
+            context.bot.sendMessage(chat_id=update.message.chat_id, text=f'Nimi vaihdettu.')
+            return
+    # The group has not been added yet so might as well add it here
+    groups[newName] = update.message.chat_id
+    writeToFile()
+    context.bot.sendMessage(chat_id=update.message.chat_id, text=f'Ryhmää lisätty.')
+
+
+def help(update: Update, context: CallbackContext):
+    helpText = f'Lisää ryhmä: /addgroup <nimi>\n' \
+        f'Vaihda tämän ryhmän nimeä: /changename <uusi_nimi>\n' \
+        f'Listaa kaikki ryhmät: /list\n' \
+        f'Välitä viesti toiselle viestille: @ryhmä viestisi'
+    context.bot.sendMessage(chat_id=update.message.chat_id, text=helpText)
+
+
+updater = Updater(token=config.TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler('addgroup', addGroup))
 dispatcher.add_handler(CommandHandler('list', printGroups))
-dispatcher.add_handler(MessageHandler(Filters.text, forwardMsg))
+dispatcher.add_handler(CommandHandler('changename', changeName))
+dispatcher.add_handler(CommandHandler('help', help))
+dispatcher.add_handler(MessageHandler(Filters.reply & Filters.entity('mention'), replyForward))
+dispatcher.add_handler(MessageHandler(Filters.entity('mention'), forwardMsg))
 
 updater.start_polling()
